ffbb932809d283b28d0d3199c9163b45c870cf02
Gabriel Accetta <89519105+gabscrobson@users.noreply.github.com>
2022-06-21 09:47:28 -0300

    Update script.js

--- template/script.js
+++ template/script.js
@@ -1,18 +1,1 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } 
-  
-  else {
-    document.getElementById("navbar").style.top = "-70px";
-  }
-  
-  if(prevScrollpos != currentScrollPos) {
-    document.getElementById("menuToggle").checked = false;
-  }
-
-  prevScrollpos = currentScrollPos;
-}
 
